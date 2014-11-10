1a3103193de07b15fde9dd2565d3bde4384578e7
billyp <billy.poggi@gmail.com>
2014-11-09 21:35:18 -0500

    Learning is Occuring
    Found a menu select ability from the internet.  Added it to the
    thirdfile.py to be figured out later with different text.  Currently
    runs, but does not exit loop.  Have to force quit.  Also, raw_input was
    a python 2 way to get keyboard presses.  In python 3 it has been renamed
    to just input().

--- thirdfile.py
+++ thirdfile.py
@@ -1,3 +1,26 @@
 print ("hello world")
 print ("chuck, this is billy, HI!")
 print ("this after my 2nd commit")
+
+import sys
+
+ans=True
+while ans:
+    print ("""
+    1.Add a Student
+    2.Delete a Student
+    3.Look Up Student Record
+    4.Exit/Quit
+    """)
+    ans=input("What would you like to do? ") 
+    if ans=="1": 
+      print("\n Student Added") 
+    elif ans=="2":
+      print("\n Student Deleted") 
+    elif ans=="3":
+      print("\n Student Record Found") 
+    elif ans=="4":
+      print("\n Goodbye") 
+    elif ans !="":
+      print("\n Not Valid Choice Try again") 
+
